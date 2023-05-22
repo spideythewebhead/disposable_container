2eae4927a2d218b96a6f7d35796b8d7f1db139ee
Pantelis Tsakoulis <spideythewebhead@gmail.com>
2023-05-23 00:48:19 +0300

    Add a  flutter example
    - Add example platforms folders to gitignore

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,68 +1,94 @@
+import 'package:disposable_container/disposable_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        body: Center(
+          child: Builder(
+            builder: (context) {
+              return ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => const DisposableContainerScreen()),
+                  );
+                },
+                child: const Text('Push screen with disposable container'),
+              );
+            },
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class DisposableContainerScreen extends StatefulWidget {
+  const DisposableContainerScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DisposableContainerScreen> createState() =>
+      _DisposableContainerScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _DisposableContainerScreenState extends State<DisposableContainerScreen>
+    with
+        // Use [WidgetDisposableContainerMixin] that exposes a `disposableContainer` getter
+        WidgetDisposableContainerMixin {
+  final TextEditingController _textEditingController =
+      TextEditingController(text: 'Initial text');
+
+  // Use [autoDisposeWith] extension to dispose with a diposableContainer
+  late final FocusNode focusNode =
+      FocusNode().autoDisposeWith(disposableContainer);
+
   int _counter = 0;
 
+  @override
+  void initState() {
+    super.initState();
+
+    disposableContainer.addDisposable(_textEditingController.dispose);
+
+    Stream.periodic(const Duration(seconds: 1))
+        .listen((event) => _incrementCounter())
+        // or use disposableContainer.addSubscription(Stream.listen(..))
+        .autoCancelWith(disposableContainer);
+
+    disposableContainer
+        .addDisposable(() => debugPrint('I am being disposed.. bye!'));
+  }
+
   void _incrementCounter() {
+    debugPrint('Incrementing counter to ${_counter + 1}');
     setState(() {
-      _counter++;
+      _counter += 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      appBar: AppBar(),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: _textEditingController,
+          ),
+          const SizedBox(height: 12),
+          Text('$_counter'),
+        ],
       ),
     );
   }
